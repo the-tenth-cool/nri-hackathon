f3595f7578d9520e8f807f96a00b261d78aa062d
SugitaMotoki <ce235019@meiji.ac.jp>
2024-11-10 19:10:02 +0900

    2種類のカードのリクエスト機能を追加

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,20 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  vite: {
-    server: {
-    },
-  },
+  /**
+   * Nuxtアプリで使用するコンフィグ
+   */
+  runtimeConfig: {
+    /**
+     * 全コンポーネントから使えるもの
+     */
+    public: {
+      /**
+       * APIサーバのオリジン
+       */
+      apiOrigin: "http://localhost:8000",
+    }
+  }
 })
-
-module.exports = {
-  devServer: {
-    proxy: {
-      '/available_cards': {
-        target: 'http://localhost:8000',  // バックエンドのURL
-        changeOrigin: true,  // オリジンをバックエンドに一致させる
-        pathRewrite: { '^/available_cards': '/available_cards' },  // 必要に応じてパスを書き換える
-      },
-    },
-  },
-};
